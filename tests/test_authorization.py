5740123808a20df706cf8e4870774be357018c09
Leshaaa13 <alexded2003@mail.ru>
2026-06-08 18:56:20 +0300

    Плагины в Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,18 +4,18 @@ import pytest
 @pytest.mark.regression
 @pytest.mark.authorization
 
-def test_wrong_email_or_password_authorization(chromiun_page: Page):
-    chromiun_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+def test_wrong_email_or_password_authorization(chromium_page: Page):
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = chromiun_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
-    password_input = chromiun_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill('password')
 
-    login_button = chromiun_page.get_by_test_id('login-page-login-button')
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
 
-    wrong_email_or_password_alert = chromiun_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -3,21 +3,21 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromiun_page:Page):
-        chromiun_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+def test_successful_registration(chromium_page:Page):
+        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-        email_input = chromiun_page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
         email_input.fill('user.name@gmail.com')
 
-        user_input = chromiun_page.get_by_test_id('registration-form-username-input').locator('input')
+        user_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
         user_input.fill('username')
 
-        password_input = chromiun_page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
         password_input.fill('password')
 
-        registration_button = chromiun_page.get_by_test_id('registration-page-registration-button')
+        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
         registration_button.click()
 
 
-        dashboard_tittle = chromiun_page.get_by_test_id('dashboard-toolbar-title-text')
+        dashboard_tittle = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
         expect(dashboard_tittle).to_be_visible()
