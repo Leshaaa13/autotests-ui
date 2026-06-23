b42bc12849b03841da9ba822ffc1aded64f60f76
Leshaaa13 <alexded2003@mail.ru>
2026-06-24 01:07:27 +0300

    Знакомство с PageComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,5 +1,3 @@
-from operator import index
-
 from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
@@ -13,7 +11,7 @@ class CoursesListPage(BasePage):
 
         self.course_title = page.get_by_test_id('course-widget-title-text')
         self.course_image = page.get_by_test_id('course-preview-image')
-        self.course_max_text = page.get_by_test_id('course-min-score-info-row-view-text')
+        self.course_max_text = page.get_by_test_id('course-max-score-info-row-view-text')
         self.course_min_text = page.get_by_test_id('course-min-score-info-row-view-text')
         self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
 
@@ -43,19 +41,19 @@ class CoursesListPage(BasePage):
 
     def click_create_course_button(self):
         expect(self.create_course_button).to_be_visible()
-
+        self.create_course_button.click()
 
     def check_visible_course_card(self, title: str, max_score: str, min_score: str, estimated_time: str, index: int):
         expect(self.course_image.nth(index)).to_be_visible()
 
         expect(self.course_title.nth(index)).to_be_visible()
-        expect(self.course_title.nth(index)).to_have_text('title')
+        expect(self.course_title.nth(index)).to_have_text(title)
 
         expect(self.course_max_text.nth(index)).to_be_visible()
-        expect(self.course_max_text.nth(index)).to_have_text(f'Max score :{max_score}')
+        expect(self.course_max_text.nth(index)).to_have_text(f'Max score: {max_score}')
 
         expect(self.course_min_text.nth(index)).to_be_visible()
-        expect(self.course_min_text.nth(index)).to_have_text(f'Min score :{min_score}')
+        expect(self.course_min_text.nth(index)).to_have_text(f'Min score: {min_score}')
 
         expect(self.course_estimated_time_text.nth(index)).to_be_visible()
         expect(self.course_estimated_time_text.nth(index)).to_have_text(f'Estimated time: {estimated_time}')
--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,14 +1,15 @@
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
+from components.navigation.navbar_component import NavbarComponent
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
-
         super().__init__(page)
+        self.navbar = NavbarComponent(page)
 
         self.dashboard_title = page.get_by_test_id(
-            "dashboard-toolbar-title-text"
-        )
+            "dashboard-toolbar-title-text")
+
 
         self.students_title = page.get_by_test_id('students-widget-title-text')
         self.students_chart = page.get_by_test_id('students-bar-chart')
@@ -24,11 +25,11 @@ class DashboardPage(BasePage):
 
 
 
-    def check_dashboard_title_visible(self):
+    def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
 
-    def check_visible_dashboard_title(self):
+    def check_visible_students_chart(self):
         expect(self.students_title).to_be_visible()
         expect(self.students_title).to_have_text('Students')
         expect(self.students_chart).to_be_visible()
@@ -40,10 +41,10 @@ class DashboardPage(BasePage):
 
     def check_visible_courses_chart(self):
         expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Coureses')
+        expect(self.courses_title).to_have_text('Courses')
         expect(self.students_chart).to_be_visible()
 
     def check_visible_scores_chart(self):
         expect(self.scores_title).to_be_visible()
         expect(self.scores_title).to_have_text('Scores')
-        expect(self.sscores_title).to_be_visible()
+        expect(self.scores_title).to_be_visible()
